4a9f25a326ca0a13744fcc2fc0334ae35111cb89
byxir <egor.solo1@yandex.ru>
2023-10-22 03:50:02 +0200

    fix path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="">
-      <iframe src="../org.pdf" width="100%" height="100vh" />
+      <iframe src="../org.pdf" className="h-screen w-full" />
     </div>
   );
 }
